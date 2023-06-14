efcbd31d10a77beae5c55e8062d1e7cf8c9597d1
Elgun <Elgun@DESKTOP-NLQ92MK>
2023-06-14 08:47:14 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DateTime date = DateTime.Now;
+            List<Message> messages = new List<Message>() 
+            {
+                new Message() {message="Hello , How Are you today ?",senttime=date.TimeOfDay}
+            };
+            DataContext = messages;
+        }
+
+        private void DataTemplate_DpiChanged(object sender, DpiChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WpfApp1/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Message
+    {
+        public Message()
+        {
+            
+        }
+        public string message { get; set; }
+        public TimeSpan senttime { get; set; }
+    }
+}
